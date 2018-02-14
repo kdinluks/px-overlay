58246e99c5a263e168276495c02b7bebe5b98196
Elliot Nahman <elliot.nahman@ge.com>
2018-02-14 12:14:39 -0800

    updates based on code review; switch to using maps; clean up some bugs; get rid of ids

--- test/px-overlay-custom-tests.js
+++ test/px-overlay-custom-tests.js
@@ -1,60 +1,47 @@
-suite('Custom Automation Tests for px-overlay', function() {
-  let overlay;
-  let salmonOverlay;
+suite('Basic configuration', function() {
+  let overlayContent,
+      overlayContainer,
+      overlayHost,
+      contentEvents = [],
+      windowEvents = [];
 
-  setup((done)=>{
-    overlay = fixture('px-overlay-fixture');
-    salmonOverlay = fixture('px-overlay-salmon-fixture');
-    flush(()=>{
-      done();
-    })
+  suiteSetup(() => {
+    overlayContent = document.getElementById('px-overlay-content-fixture');
+    overlayContainer = document.getElementById('px-overlay-container-fixture');
+
+    overlayHost = overlayContainer.shadowRoot ? overlayContainer.shadowRoot.querySelector('#overlayHost') : overlayContainer.querySelector('#overlayHost');
+
+    document.getElementById('eventCatcher').addEventListener('test-event', evt => { contentEvents.push(evt); });
+    window.addEventListener('test-event', evt => { windowEvents.push(evt); });
   });
 
-  test('Overlay is correct background color', function(done) {
-    let overlayDiv = Polymer.dom(overlay.root).querySelector('div');
-    async.until(
-      function() {
-        return (window.getComputedStyle(overlayDiv).backgroundColor.substring(0,18) === 'rgba(0, 0, 0, 0.65');
-      },
-      function(callback) {
-        setTimeout(callback, 1000);
-      },
-      function (err, n) {
-        assert.equal(window.getComputedStyle(overlayDiv).backgroundColor.substring(0,18), 'rgba(0, 0, 0, 0.65');
-        done();
-      }
-    )
+  test('Content is hoisted to container', function() {
+    const elems = overlayHost.querySelectorAll('div');
+
+    assert.equal(elems.length, 1);
+    assert.equal(elems[0].children[0].textContent, "HI DAVID");
   });
 
-  test('Overlay is correct size', function(done) {
-    async.until(
-      function() {
-        return (parseInt(window.getComputedStyle(overlay).height) === window.innerHeight);
-      },
-      function(callback) {
-        setTimeout(callback, 1000);
-      },
-      function (err, n) {
-        assert.equal(parseInt(window.getComputedStyle(overlay).height), window.innerHeight);
-        assert.equal(parseInt(window.getComputedStyle(overlay).width), window.innerWidth);
-        done();
-      }
-    )
+  test('Event is retargeted', function() {
+    const h1 = overlayHost.querySelector('h1');
+
+    const event = new CustomEvent('test-event', {
+      bubbles: true,
+      composed: true,
+      cancelable: true,
+      detail: {hi: "david"}
+    });
+
+    h1.dispatchEvent(event);
+
+    assert.equal(contentEvents.length, 1);
+    assert.equal(windowEvents.length, 1);
+
+    assert.deepEqual(contentEvents[0].detail, {hi: "david"});
+    debugger
+    assert.deepEqual(contentEvents[0].sourceEvent.rootTarget, );
+
+
   });
 
-  test('Overlay accepts style variable', function(done) {
-    let div = Polymer.dom(salmonOverlay.root).querySelector('#overlay');
-    async.until(
-      function() {
-        return (window.getComputedStyle(div).backgroundColor === 'rgb(250, 128, 114)');
-      },
-      function(callback) {
-        setTimeout(callback, 1000);
-      },
-      function (err, n) {
-        assert.equal(window.getComputedStyle(div).backgroundColor, 'rgb(250, 128, 114)');
-        done();
-      }
-    )
-  });
 });
